365e2dcea16ac0ed57c1068c766edc913dd1c699
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-04-12 22:09:59 +0700

    struktur folder

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Halaman About</title>
+</head>
+<body>
+    <div>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+    </div>
+    <h1>Owner : {{ $nama }}</h1>
+    <h1>Halaman About</h1>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Halaman Home</title>
+</head>
+<body>
+    <div>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+    </div>
+    <h1>Halaman Home</h1>
+    <img src="img/Go_Youn-jung.png" alt="profile go youn jung">
+    <script src="js/script.js"></script>
+</body>
+</html>
