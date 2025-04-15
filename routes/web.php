4216d9ebb137bdb088404036fbe74d8216162db2
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-04-16 02:58:56 +0700

    learn : Model

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Arr;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home');
@@ -10,56 +11,12 @@ Route::get('/about', function () {
     return view('about', ["nama" => "Rizky Ardiansyah"]);
 });
 Route::get('/blog', function () {
-    return view('blog', ['posts' => [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => "Judul Artikel 1",
-            'author' => 'Rizky Ardiansyah',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ducimus iure impedit error nostrum sint
-            eos numquam, ex saepe inventore doloremque praesentium minus voluptatum illum, magnam enim ipsa dignissimos
-            vero.',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => "Judul Artikel 2",
-            'author' => 'Rizky Ardiansyah pasaribu',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo expedita, ab similique quaerat aut laboriosam
-            eum dolor velit beatae! Recusandae harum vel est porro consequuntur fugiat, assumenda quam voluptates
-            eligendi.',
-        ],
-    ]]);
+    return view('blog', ['posts' => Post::all()]);
 });
 Route::get('/contact', function () {
     return view('contact');
 });
 
 Route::get('/blog/{slug}', function($slug){
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => "Judul Artikel 1",
-            'author' => 'Rizky Ardiansyah',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ducimus iure impedit error nostrum sint
-            eos numquam, ex saepe inventore doloremque praesentium minus voluptatum illum, magnam enim ipsa dignissimos
-            vero.',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => "Judul Artikel 2",
-            'author' => 'Rizky Ardiansyah pasaribu',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo expedita, ab similique quaerat aut laboriosam
-            eum dolor velit beatae! Recusandae harum vel est porro consequuntur fugiat, assumenda quam voluptates
-            eligendi.',
-        ],
-    ];
-
-    $post = Arr::first($posts, function($post) use ($slug){
-        return $post['slug'] == $slug;
-    });
-
-    return view('post', ['post' => $post]);
+    return view('post', ['post' => Post::find($slug)]);
 });
